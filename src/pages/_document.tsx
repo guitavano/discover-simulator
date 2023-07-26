0aa6f7462c841039199c096aded5939b97e1847b
guitavano <tavano62@gmail.com>
2023-07-25 21:07:31 -0300

    remove crossorigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta name="google-site-verification" content="SP-NDt1OTP6iZ_yeFKjE-GXkC6GXnJ-Cp-KSmbARGTc" />
         <link rel="icon" type="image/x-icon" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet" />
         <link rel="preload" as="image" href="/bg1.png" />
         <link rel="preload" as="image" href="/bg2.png" />

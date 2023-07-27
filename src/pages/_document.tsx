5a77f155b202d4e3163c2dbb03fdf92eb97585cf
guitavano <tavano62@gmail.com>
2023-07-27 09:28:14 -0300

    fix favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="SP-NDt1OTP6iZ_yeFKjE-GXkC6GXnJ-Cp-KSmbARGTc" />
-        <link rel="icon" type="image/x-icon" href="/icon.png" />
+        <link rel="icon" type="image/png" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet" />

c450b6665541c7762c5839c9513bbd4eb22ec71f
guitavano <tavano62@gmail.com>
2023-07-28 14:49:24 -0300

    add favicon.ico

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <meta name="google-site-verification" content="SP-NDt1OTP6iZ_yeFKjE-GXkC6GXnJ-Cp-KSmbARGTc" />
         <link rel="icon" type="image/png" href="/icon.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&display=swap" rel="stylesheet" />
